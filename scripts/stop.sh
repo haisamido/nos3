13a17717f991a4ede567741b7ffd5a2c09247aeb
Mark Suder <mark.d.suder@nasa.gov>
2025-03-04 15:39:30 -0500

    https://github.com/nasa/nos3/issues/300 - Clean up the shared memory segment on stop so that it does not hang the system the next time nos3 is run.

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -24,6 +24,8 @@ $DCALL ps --filter=name="influxdb*" -aq | xargs $DCALL stop > /dev/null 2>&1 &
 $DCALL ps --filter=name="ttc-command*" -aq | xargs $DCALL stop > /dev/null 2>&1 &
 $DCALL ps --filter ancestor="ballaerospace/cosmos:4.5.0" -aq | xargs $DCALL stop > /dev/null 2>&1 &
 
+rm -rf /dev/shm/Blackboard
+
 # Intentionally wait to complete
 wait 
 
