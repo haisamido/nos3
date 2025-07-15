3435c687969c27297671963a9d5a9bbbef111b60
Haisam Ido <haisam.ido@nasa.gov>
2025-07-15 09:18:06 -0400

    added comment about .env variable and yamcs/entrypoint.sh was simplified

--- deployments/docker/yamcs/entrypoint.sh
+++ deployments/docker/yamcs/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+pkill -9 java
 rm -rf ${YAMCS_DATA_DIR}/_global.rdb/LOCK
+rm -rf ${YAMCS_DATA_DIR}/nos3.rdb/LOCK
 
-mvn -Dmaven.repo.local=${MAVEN_REPO_LOCAL} -DCOMPONENT_DIR=${COMPONENT_DIR} yamcs:run
+#mvn ${MAVEN_HTTPS_PROXY} -Dmaven.repo.local=${MAVEN_REPO_LOCAL} -DCOMPONENT_DIR=${COMPONENT_DIR} yamcs:run
+
+mvn -DCOMPONENT_DIR=${COMPONENT_DIR} yamcs:run
 
 tail -f /dev/null
